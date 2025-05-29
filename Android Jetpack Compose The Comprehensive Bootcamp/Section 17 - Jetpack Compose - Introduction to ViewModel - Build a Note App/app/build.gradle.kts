c494c65b134f45c233c80e2132b43906499d4923
SAKIF RAHMAN MISHAD <109865291+mishad01@users.noreply.github.com>
2025-05-30 01:53:04 +0600

    feat: implement Android ROOM with Dependency Injection using Hilt & Dagger
    - Covered concepts of Dependency Injection and its advantages
    - Compared manual DI vs Hilt
    - Configured Hilt dependencies in Gradle
    - Reviewed essential dependencies and setup process

--- Android Jetpack Compose The Comprehensive Bootcamp/Section 17 - Jetpack Compose - Introduction to ViewModel - Build a Note App/app/build.gradle.kts
+++ Android Jetpack Compose The Comprehensive Bootcamp/Section 17 - Jetpack Compose - Introduction to ViewModel - Build a Note App/app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.devtools.ksp")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -57,4 +59,7 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation("com.google.dagger:hilt-android:2.56.2")
+    ksp("com.google.dagger:hilt-android-compiler:2.56.2")
+
 }
